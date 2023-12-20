aa3ed28338ead3b98db6a4591e8ac54b2cc9b6b6
Anton <115624138+ppljc@users.noreply.github.com>
2023-12-20 11:46:55 +0600

    added kb_inline_SelectGroup

--- keyboards/kb_group.py
+++ keyboards/kb_group.py
@@ -3,8 +3,15 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 # <---------- Inline клавиатуры ---------->
-def kb_inline_connectGroup(groups: list):
+def kb_inline_ConnectGroup(groups: list):
 	button = InlineKeyboardButton(text='👥 Привязать группу', callback_data=f'ConnectGroup {groups}')
 	reply_markup = InlineKeyboardMarkup(row_width=1)
 	reply_markup.add(button)
+	return reply_markup
+
+def kb_inline_SelectGroup(groups: list):
+	reply_markup = InlineKeyboardMarkup(row_width=2)
+	for group_id, group_name in groups:
+		button = InlineKeyboardButton(text=group_name, callback_data=f'ChoosedGroup {group_id}')
+		reply_markup.row(button)
 	return reply_markup
