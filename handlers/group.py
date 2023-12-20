119050c372c9b3b7690b3c43fe2ab271a0e60461
ppljc <tony_shmidt@mail.ru>
2023-12-21 00:23:09 +0600

    updated and fixed all process from adding bot to chat and linking this chat to group

--- handlers/group.py
+++ handlers/group.py
@@ -10,7 +10,9 @@ from data_base.db_psql import *
 from messages.ms_group import *
 from keyboards.kb_group import *
 from utilities.ut_logger import ut_LogCreate
+from utilities.ut_security import ut_EncodeLink
 from utilities.ut_pyrogrambot import ut_GetChatMembers
+from data_base.operation import psql
 
 
 # <---------- Переменные ---------->
@@ -20,45 +22,51 @@ filename = 'group.py'
 # <---------- Callback функции ---------->
 async def group_callback_SelectGroup(query: types.CallbackQuery):
 	try:
-		groups = list(query.data[18:])
+		groups = query.data.split('|')[1:]
 		await bot.edit_message_text(
 			chat_id=query.message.chat.id,
 			message_id=query.message.message_id,
 			text=msgr_SelectGroup,
-			reply_markup=kb_inline_SelectGroup(groups)
+			reply_markup=(await kb_inline_SelectGroup(groups=groups))
 		)
 		await ut_LogCreate(
 			id=query.from_user.id,
 			filename=filename,
 			function='group_callback_SelectGroup',
 			exception='',
-			content=f'There are {len(groups)} groups: {gropus}.'
+			content=f'There are this groups: {groups}.'
 		)
 	except Exception as exception:
 		await ut_LogCreate(
 			id=query.from_user.id,
 			filename=filename,
 			function='group_callback_SelectGroup',
-                        exception=exception,
+			exception=exception,
 			content=''
 		)
 
 
 async def group_callback_BindChatSettings(query: types.CallbackQuery):
 	try:
-		group_id = query.data[18:]
+		group = query.data.split('|')
+		group_id = int(group[1])
+		group_name = group[2]
+		reply_markup = await kb_inline_ChatSettings(
+			group_id=group_id,
+			group_name=group_name
+		)
 		await bot.edit_message_text(
 			chat_id=query.message.chat.id,
 			message_id=query.message.message_id,
-			text=msgr_ChatSettins,
-			reply_markup=kb_inline_ChatSettings
+			text=msgr_ChatSettings,
+			reply_markup=reply_markup
 		)
 		await ut_LogCreate(
 			id=query.from_user.id,
 			filename=filename,
 			function='group_callback_BindChatSettings',
 			exception='',
-			content=f'Choosed group with id={group_id}'
+			content=f'Chosen group with id={group[1]}'
 		)
 	except Exception as exception:
 		await ut_LogCreate(
@@ -70,31 +78,141 @@ async def group_callback_BindChatSettings(query: types.CallbackQuery):
 		)
 
 
+async def group_callback_BindGroup(query: types.CallbackQuery):
+	try:
+		group = query.data.split('|')
+		group_id = int(group[1])
+		group_name = group[2]
+		notifications = bool(group[3])
+		response = await db_psql_InsertChat(
+			db=psql,
+			id=query.message.chat.id,
+			title=query.message.chat.title,
+			group_id=group_id,
+			notifications=notifications,
+		)
+		content = ''
+		exception = ''
+		if response:
+			link = await ut_EncodeLink(
+				group_id=group_id,
+				id=query.from_user.id
+			)
+			await PostgreSQL.update(
+				self=psql,
+				table='groups',
+				what='group_link',
+				what_value=link,
+				where='group_id',
+				where_value=group_id
+			)
+			text = await msgr_GroupBound(group_name=group_name)
+			reply_markup = await kb_inline_GroupLink(
+				group_id=group_id,
+				group_name=group_name,
+				link=link
+			)
+			content = f'Chat with id={query.message.chat.id}, title={query.message.chat.title} bound to group with group_id={group_id}, group_name={group_name}.'
+		else:
+			text = msgr_GroupBindError
+			reply_markup = kb_inline_ReloadChat(message=query.message)
+			exception = f'Can`t bind chat with id={query.message.chat.id}, title={query.message.chat.title} to group with group_id={group_id}, group_name={group_name}.'
+		await bot.send_message(
+			chat_id=query.message.chat.id,
+			text=text,
+			reply_markup=reply_markup
+		)
+		await ut_LogCreate(
+			id=query.from_user.id,
+			filename=filename,
+			function='group_callback_BindGroup',
+			content=content,
+			exception=exception
+		)
+	except Exception as exception:
+		await ut_LogCreate(
+			id=query.from_user.id,
+			filename=filename,
+			function='group_callback_BindGroup',
+			exception=exception,
+			content=''
+		)
+
+
+async def group_callback_ReloadChat(query: types.CallbackQuery):
+	try:
+		response = await PostgreSQL.delete(
+			self=psql,
+			table='chats',
+			where='id',
+			where_value=query.message.chat.id
+		)
+		content = ''
+		exception = ''
+		if response:
+			await bot.send_message(
+				chat_id=query.message.chat.id,
+				text=msgr_ChatReloaded
+			)
+			await group_handler_ChatStart(message=query.message)
+			content = f'Chat with id={query.message.chat.id}, title={query.message.chat.title} was deleted from chats table.'
+		else:
+			await bot.send_message(
+				chat_id=query.message.chat.id,
+				text=msgr_ChatReloadError
+			)
+			exception = f'Can`t delete chat with id={query.message.chat.id}, title={query.message.chat.title} from chats table.'
+		await ut_LogCreate(
+			id=query.from_user.id,
+			filename=filename,
+			function='group_callback_ReloadChat',
+			exception=exception,
+			content=content
+		)
+	except Exception as exception:
+		await ut_LogCreate(
+			id=query.from_user.id,
+			filename=filename,
+			function='group_callback_ReloadChat',
+			exception=exception,
+			content=''
+		)
+
+
 # <---------- Handler функции ---------->
 async def group_handler_ChatStart(message: types.Message):
 	try:
 		if message.chat.type == 'group' or message.chat.type == 'supergroup':
 			if message.from_user.bot.id == await bot.get_me():
 				chat_members = await ut_GetChatMembers(chat_id=message.chat.id)
-				groups = []
+				group_ids = []
+				group_names = []
 				for id in chat_members:
 					group_id = await PostgreSQL.select(
+						self=psql,
 						table='users',
 						what='group_id',
 						where='id',
 						where_value=id
 					)
-					group_name = await PostgreSQL.select(
-					        table='groups',
-						what='group_name',
-						where='group_id',
-						where_value=group_id
-					)
-                                        groups.append((group_id, group_name,))
+					if group_id and (group_id not in group_ids):
+						group_name = await PostgreSQL.select(
+							self=psql,
+							table='groups',
+							what='group_name',
+							where='group_id',
+							where_value=group_id
+						)
+						group_ids.append(group_id)
+						group_names.append(group_name)
+				reply_markup = await kb_inline_ConnectGroup(
+					group_ids=group_ids,
+					group_names=group_names
+				)
 				await bot.send_message(
 					chat_id=message.chat.id,
 					text=msgr_ChatStart,
-					reply_markup=kb_inline_ConnectGroup(groups)
+					reply_markup=reply_markup
 				)
 				await ut_LogCreate(
 					id=00000000,
@@ -128,4 +246,5 @@ async def group_handler_ChatStart(message: types.Message):
 def register_handlers_group(dp: Dispatcher):
 	dp.register_message_handler(group_handler_ChatStart, content_types=ContentType.NEW_CHAT_MEMBERS)
 	dp.register_callback_query_handler(group_callback_SelectGroup, Text(startswith='ConnectGroup'))
-	dp.register_callback_query_handler(group_callback_BindChatSettings, Text(startswith='ChoosedGroup'))
+	dp.register_callback_query_handler(group_callback_BindChatSettings, Text(startswith='ChosenGroup'))
+	dp.register_callback_query_handler(group_callback_BindGroup, Text(startswith='ChatSettings'))
