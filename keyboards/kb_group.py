78fa3f10b16e9f5e3b3b170cda1fcc841ca74f91
ppljc <tony_shmidt@mail.ru>
2023-12-21 00:25:15 +0600

    updated for group handlers

--- keyboards/kb_group.py
+++ keyboards/kb_group.py
@@ -2,16 +2,50 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
+# <---------- Импорт сторонних функций ---------->
+from json import loads
+
+
+# <---------- Импорт функций Aiogram ---------->
+from utilities.ut_security import ut_EncodeLink
+
+
 # <---------- Inline клавиатуры ---------->
-def kb_inline_ConnectGroup(groups: list):
-	button = InlineKeyboardButton(text='👥 Привязать группу', callback_data=f'ConnectGroup {groups}')
+async def kb_inline_ConnectGroup(group_ids: list, group_names: list):
+	button = InlineKeyboardButton(text='👥 Привязать группу', callback_data=f'ConnectGroup|{group_ids}|{group_names}')
 	reply_markup = InlineKeyboardMarkup(row_width=1)
-	reply_markup.add(button)
+	reply_markup.row(button)
 	return reply_markup
 
-def kb_inline_SelectGroup(groups: list):
+
+async def kb_inline_SelectGroup(groups: list):
 	reply_markup = InlineKeyboardMarkup(row_width=2)
-	for group_id, group_name in groups:
-		button = InlineKeyboardButton(text=group_name, callback_data=f'ChoosedGroup {group_id}')
+	group_ids = loads(groups[0])
+	group_names = loads(groups[1])
+	for i in range(len(group_ids)):
+		button = InlineKeyboardButton(text=group_names[i], callback_data=f'ChosenGroup|{group_ids[i]}|{group_names[i]}')
 		reply_markup.row(button)
+	return reply_markup
+
+
+async def kb_inline_ChatSettings(group_id: int, group_name: str):
+	button0 = InlineKeyboardButton(text='🔔 Включить', callback_data=f'ChatSettings|{group_id}|{group_name}|True')
+	button1 = InlineKeyboardButton(text='🔕 Выключить', callback_data=f'ChatSettings|{group_id}|{group_name}|False')
+	reply_markup = InlineKeyboardMarkup(row_width=2)
+	reply_markup.row(button0, button1)
+	return reply_markup
+
+
+async def kb_inline_GroupLink(group_id: int, group_name: str, link: str):
+	button0 = InlineKeyboardButton(text=f'📲 Вступить в {group_name}', url=link)
+	button1 = InlineKeyboardButton(text='🧨 Удалить ссылку', callback_data=f'DeleteLink|{group_id}')
+	reply_markup = InlineKeyboardMarkup(row_width=1)
+	reply_markup.row(button0, button1)
+	return reply_markup
+
+
+async def kb_inline_ReloadChat(message):
+	button0 = InlineKeyboardButton(text='▶️ Попробовать снова', callback_data=f'ReloadChat|{message}')
+	reply_markup = InlineKeyboardMarkup(row_width=1)
+	reply_markup.row(button0)
 	return reply_markup
