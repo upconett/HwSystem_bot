c09345c85c671c8622379257b8d488f3db2c9873
StepanTap <stepantarabin@mail.ru>
2024-01-06 11:47:28 +0600

    router fixes

--- handlers/routers.py
+++ handlers/routers.py
@@ -13,24 +13,24 @@ router_base = Router()
 # <---------- Private routers ---------->
 #     <- Router for private chat ->
 router_private = Router()
-router_private.message(ChatType(['private']))
+router_private.message.filter(ChatType(['private']))
 
 #     <- Router for private chat and group admin ->
 router_private_groupAdmin = Router()
-router_private_groupAdmin.message(ChatType(['private']), UserIsGroupAdmin(flag=True))
+router_private_groupAdmin.message.filter(ChatType(['private']), UserIsGroupAdmin(flag=True))
 
 
 # <---------- Group/supergroup routers ---------->
 #     <- Router for private chat ->
 router_chat = Router()
-router_chat.message(ChatType(['group', 'supergroup']))
+router_chat.message.filter(ChatType(['group', 'supergroup']))
 
 
 # <---------- Reg/unreg routers ---------->
 #     <- Router for registered users ->
 router_registered = Router()
-router_registered.message(UserRegister(flag=True))
+router_registered.message.filter(UserRegister(flag=True))
 
 #     <- Router for unregistered users ->
 router_unregistered = Router()
-router_unregistered.message(UserRegister(flag=False))
+router_unregistered.message.filter(UserRegister(flag=False))
